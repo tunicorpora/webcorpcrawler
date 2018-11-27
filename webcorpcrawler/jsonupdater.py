8035d31184aa964642defab323293513064f42c8
Juho Härme <juho.harme@gmail.com>
2018-11-27 14:11:40 +0200

    added functionality for adding parsed data

--- webcorpcrawler/jsonupdater.py
+++ webcorpcrawler/jsonupdater.py
@@ -3,8 +3,11 @@
 import json
 import glob
 import os.path
+import os
 import uuid
 import progressbar
+import re
+import sys
 
 
 class JsonUpdater():
@@ -15,6 +18,10 @@ class JsonUpdater():
         -folder: path to folder containing json files
         """
 
+        if not files:
+            print("You have to provide the target / source files of the action")
+            sys.exit(2)
+
         self.files = []
         self.data = []
         if len(files) == 1 and os.path.isdir(files[0]):
@@ -23,19 +30,88 @@ class JsonUpdater():
                 self.files.append(fname)
         else:
             self.files = files
+        self.ReadData()
 
+
+    def ReadData(self):
+        """
+        Reads the json files to memory
+        """
+        print("Reading files...")
+        for fname in progressbar.progressbar(self.files):
+            with open(fname, "r") as f:
+                data = json.load(f)
+                self.data.append({"data": data, "fname": fname})
 
     def AddUids(self):
         """
         Adds unique indices to crawled entries and updates the json files
         """
-        print("indexing files...")
-        for fname in progressbar.progressbar(self.files):
-            with open(fname, "r") as f:
-                data = json.load(f)
-            for idx, item in enumerate(data):
-                data[idx]["id"] = str(uuid.uuid4())
-            self.data.append({"data": data, "fname": fname})
+
+        print("Adding unique indices")
+        for e_idx, entry in enumerate(self.data):
+            for i_idx, item in enumerate(entry["data"]):
+                self.data[e_idx]["data"][i_idx]["id"] = str(uuid.uuid4())
+
+    def PrepareForParsing(self, target_prop, target_dir="/tmp"):
+        """
+        Prepares some property of each item in the json for parsing using
+        unique identifiers
+
+        - target_prop:  what us the property that contains the string to be parsed
+        """
+
+        if not target_prop:
+            print("Please define the name of the property that the parsing will be based on")
+            return False
+
+        print("Preparing...")
+        ids = []
+        contents = []
+        for entry in self.data:
+            for item in entry["data"]:
+                if target_prop in item:
+                    ids.append(item["id"])
+                    contents.append(item[target_prop])
+
+        separator = {"mark" : "!", "counter" : 15}
+        separator_string = "\n" + separator["mark"] * separator["counter"] + "\n"
+
+        if not target_dir:
+            target_dir = "/tmp"
+        dirname = target_dir if target_dir[-1] == "/"  else target_dir + "/"
+        os.makedirs(dirname, exist_ok=True)
+        with open(dirname + "contents.txt","w") as f:
+            f.write(separator_string.join(contents))
+        with open(dirname + "ids.txt","w") as f:
+            f.write("\n".join(ids))
+        print("The prepared files were produced at {}".format(target_dir))
+
+    def AddParsed(self, target_prop, source_file, index_file):
+        """
+
+        - target_prop: name of the new property representing the parsed results
+        - source_file: parsed results
+        - index_file: file with indices
+        """
+        with open(source_file, "r") as f:
+            raw = f.read()
+        splitpattern = re.compile(r"\d+\t![^\n]+\n\n?"*14 + r"\d+\t![^\n]+\n\n")
+        results = splitpattern.split(raw)
+        with open(index_file, "r") as f:
+            indices = f.read().splitlines()
+        if len(results) != len(indices):
+            print("The number of indices and parsed entries does not match: {} vs. {}"
+                    .format(len(results), len(indices)))
+            sys.exit(0)
+        else:
+            res_by_id = {}
+            for idx, res in enumerate(results):
+                res_by_id[indices[idx]] = res
+            for e_idx, entry in enumerate(self.data):
+                for i_idx, item in enumerate(entry["data"]):
+                    if item["id"] in res_by_id:
+                        self.data[e_idx]["data"][i_idx][target_prop] = res_by_id[item["id"]]
 
 
     def Output(self, prettyprint=False):
--- webcorpcrawler/webcorpcrawler.py
+++ webcorpcrawler/webcorpcrawler.py
@@ -6,7 +6,7 @@ def main():
     parser = argparse.ArgumentParser(description='Fetches the results of a web corpus query to json')
     parser.add_argument('action', 
             metavar = 'action',
-            choices = ["crawl", "add_uids"], help="The action to run")
+            choices = ["crawl", "add_uids", "prepare", "add_parsed"], help="The action to run")
     parser.add_argument('-c',  '--corpus',
             metavar = 'corpus name',
             choices = ["integrum"], default="integrum",
@@ -15,6 +15,18 @@ def main():
             metavar = "json files",
             nargs = "*",
             help="path to the folder containing the json files (produced as a result of a previous crawl) OR a list of files")
+    parser.add_argument('--prop',
+            metavar = "property name",
+            help="which property of a data item (concordance) will be used in the action")
+    parser.add_argument('--indices',
+            metavar = "path",
+            help="path to the file containing indices, produced by the 'prepare' action")
+    parser.add_argument('--output_folder',
+            metavar = "path to folder",
+            help="where to put the output files")
+    parser.add_argument('--parsed_source',
+            metavar = "path",
+            help="path to a file containing the parsed results")
     parser.add_argument('--prettyprint',
             nargs = "?",
             const = True,
@@ -22,16 +34,21 @@ def main():
             help="prettyprints the json files")
 
     args = parser.parse_args()
-    updater = None
 
     if args.action == "crawl":
         print("let's crawl")
     elif args.action == "add_uids":
         updater = JsonUpdater(args.files)
         updater.AddUids()
-
-    if updater:
         updater.Output(args.prettyprint)
+    elif args.action == "prepare":
+        updater = JsonUpdater(args.files)
+        updater.PrepareForParsing(args.prop, args.output_folder)
+    elif args.action == "add_parsed":
+        updater = JsonUpdater(args.files)
+        updater.AddParsed(args.prop, args.parsed_source, args.indices)
+        updater.Output(args.prettyprint)
+
 
 if __name__ == '__main__':
     main()
